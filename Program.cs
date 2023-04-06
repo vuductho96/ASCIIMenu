e1b8ecbaa4c5427d4e9daa09cad672555c2b98d9
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-04-06 21:11:10 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,16 +4,19 @@ namespace MenuDrawing
 {
     class MenuDrawingView
     {
+
+
         static void Main(string[] args)
         {
             int choice;
 
+
             do
             {
                 Console.WriteLine("Menu");
-                Console.WriteLine("1. Draw the triangle");
-                Console.WriteLine("2. Draw the square");
-                Console.WriteLine("3. Draw the rectangle");
+                Console.WriteLine("1. Print the rectangle");
+                Console.WriteLine("2. Print the square triangle");
+                Console.WriteLine("3. Print isosceles triangle");
                 Console.WriteLine("0. Exit");
                 Console.Write("Enter your choice: ");
                 choice = Int32.Parse(Console.ReadLine());
@@ -21,16 +24,44 @@ namespace MenuDrawing
                 switch (choice)
                 {
                     case 1:
-                        Console.WriteLine("Drawing the triangle:");
-                        Console.WriteLine("******\n*****\n****\n***\n**\n*");
+
+
+                        Console.WriteLine("Drawing the square triangle:");
+                        for (int i = 1; i <= 5; i++)
+                        {
+                            for (int j = 1; j <= 7; j++)
+                            {
+                                Console.Write("*");
+                            }
+                            Console.WriteLine();
+                        }
+
+
                         break;
                     case 2:
-                        Console.WriteLine("Drawing the square:");
-                        Console.WriteLine("* * * * * *\n* * * * * *\n* * * * * *\n* * * * * *\n* * * * * *\n* * * * * *");
+                        Console.WriteLine("Drawing the square triangle:");
+                        for (int i = 1; i <= 5; i++)
+                        {
+                            for (int j = 1; j <= i; j++)
+                            {
+                                Console.Write("*");
+                            }
+                            Console.WriteLine();
+                        }
                         break;
                     case 3:
-                        Console.WriteLine("Drawing the rectangle:");
-                        Console.WriteLine("* * * * * *\n* * * * * *\n* * * * * *");
+                        for (int i = 1; i <= 7; i += 2)
+                        {
+                            for (int j = 7; j >= i; j -= 2)
+                            {
+                                Console.Write(" ");
+                            }
+                            for (int k = 1; k <= i; k++)
+                            {
+                                Console.Write("*");
+                            }
+                            Console.WriteLine();
+                        }
                         break;
                     case 0:
                         Console.WriteLine("Exiting program. Goodbye!");
